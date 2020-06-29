fc9bd0a4bed652ae749949ec8897294d66204e5b
28619426 <tienhung0182@gmail.comm>
2020-06-29 20:46:06 +0700

    Add redux persist for keep cart-items after refresh

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,15 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
-
 import rootReducer from "./root-reducer";
+import { persistStore } from "redux-persist";
 
 const middlewares = [logger];
 
-const store = createStore(
+export const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(...middlewares),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
-
-export default store;
+export const persistor = persistStore(store);
